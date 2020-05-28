886bcb9a683993d2c889526e76fa440d81c6e884
Your Name <you@example.com>
2020-05-28 11:49:45 +0600

    31

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 class Post  extends Component{
 	state = {
-		id:null,
+		post:null,
 
 	}
 
@@ -11,21 +11,35 @@ class Post  extends Component{
 	componentDidMount(){
 
 		let id = this.props.match.params.post_id
-
-		this.setState({
-				id:id
+		axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+		.then(res =>{
+			this.setState({
+				post:res.data
 			})
-
+		})
 
 		
+
+
 	}
 
 
 	render() { 
+		const  post = this.state.post ? (
+			<div className="post">
+
+			<div className="center">{this.state.post.title}</div>
+			
+			<p>{this.state.post.body}</p>
+			</div>
+		) : (
+			<div className="center">Loading post ..</div>
+		)
+
 
 		return(
 		<div className="container">
-			<h4 className="center">{this.state.id}</h4>
+			{post}
 		</div>
 	)
 	}
